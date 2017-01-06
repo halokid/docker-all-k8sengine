514442cc2be8392212a6316a3f59c7fbec215c89
t00rxx <82049406@qq.com>
2017-01-06 17:03:02 +0800

    1.0

---
+++ commands/install_laest_docker_debian8.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt-get purge "lxc-docker*"
+apt-get purge "docker.io*"
+
+sleep 3
+
+apt-get update
+apt-get install apt-transport-https ca-certificates gnupg2 -y
+
+
+sudo apt-key adv \
+       --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+       --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+       
+
+       
+       
+sleep 3
+echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
+
+
+sleep 3
+apt-cache policy docker-engine
+
+
+
+
+sleep 3
+sudo apt-get update
+sudo apt-get install sudo -y
+sudo apt-get install docker-engine -y
+sudo service docker start
+sudo docker run hello-world
+
+
+
--- commands/stop_kill_containers.sh
+++ commands/stop_kill_containers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 docker kill $(docker ps -q) 
 docker rm $(docker ps -a -q) 
