613fbf23f1487744fa2c995562ca80c2b4adbadd
r00txx <82049406@qq.com>
2017-06-19 15:41:36 +0800

    get more

--- docker0.1_coreCode/docker1.4_src_book_pratice/get.go
+++ docker0.1_coreCode/docker1.4_src_book_pratice/get.go
@@ -4,6 +4,8 @@ import (
   "io"
   "sync"
   "os"
+  "github.com/prometheus/common/log"
+  "github.com/miekg/dns"
 )
 
 type Engine struct {
@@ -64,6 +66,218 @@ func (eng *Engine) Job(name string, args ...string) *Job {
   }
   return job
 }
+
+
+repositories, err := graph.NewTagStore(path.Join(config.Root, "repositories" + driver.String), g)
+
+
+type TagStore struct {
+  path                string
+  graph               *Grath
+  Repositories        map[string]Respository
+  sync.Mutex
+  pullingPool         map[string]chan struct{}
+  pullshingPool       map[string]chan strcut{}
+}
+
+
+
+if !config.DisableNetwork {
+  job := eng.Job("init_networkdriver")
+
+  job.SetenvBool("xxxx", config.EnableIptables)
+  //.....
+
+  if err := job.Run(); err != nil {
+    return nil, err
+  }
+}
+
+
+daemon := &Daemon {
+  repository:               daemonRepo,
+  containers:               &contStore {s: make(map[string]*Container)},
+  graph:                    g,
+  repositories:             repositories,
+  idIndex:                  truncindex.NewTruncIndex([]string{}),
+  sysInfo:                  sysInfo,
+  volumns:                  volumns,
+  config:                   config,
+  driver:                   driver,
+  sysInitPath:              sysInitPath,
+  execDriver:               ed,
+  eng:                      eng,
+}
+
+
+
+eng.OnShutdown(func() {
+  if err := daemon.shutdown(); err != nil {
+    //....
+  }
+  if err := portallocator.ReleaseAll(); err != nil {
+    //...
+  }
+  if err := daemon.driver.Cleanup(); err != nil {
+    //...
+  }
+  if err := daemon.containerGraph.Close(); err != nil {
+    //...
+  }
+})
+
+
+
+func ServerApi(job *engine.Job) engine.Status {
+  if len(job.Args) == 0 {
+    //...
+  }
+  var (
+    protoAddrs = job.Args
+    chErrors = make(chan error, len(protoAddrs))    // create chan type is error
+    activaionLock = make(chan struct{})
+  )
+
+  for _, protoAddrs := range protoAddrs {
+    protoAddrParts := strings.SplitN(protoAddrs, "://", 2)
+    if len(protoAddrParts) !=  {
+      //...
+    }
+
+    go func() {     //groutine handle
+     log.Info(".....")
+      chErrors <- ListenAndServe(protoAddrParts[0], protoAddrParts[1], job)
+    } ()
+  }
+
+  for i := 0; i < len(protoAddrs); i += 1 {
+    err := <-chErrors
+    if err != nil {
+      return job.Error(err)
+    }
+  }
+
+  return  engine.StatusOK
+
+}
+
+
+m := map[string]map[string]HttpApiFunc {
+  "GET": {
+    "/events":        getEvents,
+    "/info":          getInfo,
+    "/version":       getVersion,
+  },
+
+  "POST": {
+
+  }
+}
+
+
+
+for method, routes := range m {
+  for route, fct := range routes {
+    log.Debugf(".....")
+    localRoute := route
+    localFct := fct
+    localMethod := method
+
+    f := makeHttpHandler(eng, logging, localMethod, localRoute, localFct,
+                          enableCors, version.Version(dockerVersion))
+
+    if localRoute == "" {
+      r.Methods(localMethod).HandlerFunc(f)
+    } else {
+      r.Path("")
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
