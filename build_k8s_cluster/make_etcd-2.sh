6e04268b67cbe8a1bad6a5c0ebcb819a6b5c4009
root <root@yypp>
2017-07-25 17:25:51 +0800

    xx

---
+++ build_k8s_cluster/make_etcd-2.sh
@@ -0,0 +1,10 @@
+#! /bin/sh 
+
+export NODE_NAME=etcd-host2 # 当前部署的机器名称(随便定义，只要能区分不同机器即可)
+export NODE_IP=10.86.20.63 # 当前部署的机器 IP
+export NODE_IPS="10.86.20.57 10.86.20.60 10.86.20.63" # etcd 集群所有机器 IP
+# etcd 集群各机器名称和对应的IP、端口
+export ETCD_NODES=etcd-host0=https://10.86.20.57:2380,etcd-host1=https://10.86.20.60:2380,etcd-host2=https://10.86.20.63:2380 
+
+
+
