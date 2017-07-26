19ebe8a039750638d2838a0b6595c7415cf63c31
82049406@qq.com <82049406@qq.com>
2017-07-26 14:21:58 +0800

    xx

--- build_k8s_cluster/git_push.sh
+++ build_k8s_cluster/git_push.sh
@@ -3,13 +3,9 @@
 git add *
 git commit -m "xx"
 #git push
-#git config user.name "82049406@qq.com"
-#echo -e "82049406@qq.com" | git push
 
-git config --global user.name "82049406@qq.com"
-git config --global user.email 82049406@qq.com
-
-
+#git config --global user.name "82049406@qq.com"
+#git config --global user.email 82049406@qq.com
 git push
 
 
