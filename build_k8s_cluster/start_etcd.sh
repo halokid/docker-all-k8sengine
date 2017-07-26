58c774145d7aad0da826df42de1da096d42f8e07
root <root@yypp>
2017-07-26 10:41:23 +0800

    xx

--- build_k8s_cluster/start_etcd.sh
+++ build_k8s_cluster/start_etcd.sh
@@ -1,9 +1,12 @@
 #! /bin/bash 
 
+#-------- config etcd -----------
 sudo mv etcd.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable etcd
-sudo systemctl start etcd
+sudo systemctl start etcd  
+
+#--------- run etcd  和上面分开执行 -----------
 systemctl status etcd
 
 
