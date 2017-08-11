17c397d915c7c300028fd3a6fc320ccd6e80ee2a
82049406@qq.com <82049406@qq.com>
2017-08-11 17:32:53 +0800

    xx

---
+++ build_k8s_cluster/deploy.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+#!coding=utf-8
+
+'''
+deploy k8s all in one
+'''
+
+def help():
+    '''
+    get help
+    --------------------------------------------------
+    params:
+
+    --node:     node type, like master, node1 etc.
+    --node:     node type, like master, node1 etc.
+    --------------------------------------------------
+    '''
+
+
+
+def load_env():
+    '''
+    load env for deploy k8s
+    '''
+
+
+
+def make_secret():
+    '''
+    make secret setting
+    '''
+
+
+
+
+def install_etcd():
+    '''
+    install & setting etcd 
+    '''
+
+
+
+
+def install_flannel():
+    '''
+    install & setting flannel 
+    '''
+
+
+def install_kube():
+    '''
+    install & setting kube
+    '''
+
+
+def install_dns():
+    '''
+    install kube dns 
+    '''
+
+
+def install_dashboard():
+    '''
+    install kube dashboard
+    '''
+
+
+def install_heapster():
+    '''
+    install heapster
+    '''
+
+
+
+def install_elk():
+    '''
+    install elk
+    '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
